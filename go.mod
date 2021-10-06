ddb593360e64c415e7425bf6c86e0f17b3ab5128
kenriortega <kenriortega@gmail.com>
2021-10-06 14:41:51 -0400

    feat: add method for parsing ints on tx result

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require github.com/hashicorp/go-retryablehttp v0.7.0
 
-require github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+require (
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/shopspring/decimal v1.2.0
+)
--- pkg/web3/utils.go
+++ pkg/web3/utils.go
@@ -1,9 +1,13 @@
 package web3
 
 import (
+	"math/big"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/hashicorp/go-retryablehttp"
+	"github.com/shopspring/decimal"
 )
 
 func handleAPIErrorResponse(res *http.Response) error {
@@ -41,4 +45,75 @@ func (c *apiClient) handleRequest(req *http.Request) (res *http.Response, err er
 	}
 
 	return res, nil
+}
+
+func convHexToDec(hexa string) (string, error) {
+	hexa = hexa[2:]
+	decimal, err := strconv.ParseInt(hexa, 16, 64)
+
+	if err != nil {
+		return "0", err
+	}
+	stringValue := strconv.FormatInt(decimal, 10)
+	return stringValue, nil
+}
+
+// IsValidAddress validate hex address
+func IsValidAddress(iaddress interface{}) bool {
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+	switch v := iaddress.(type) {
+	case string:
+		return re.MatchString(v)
+
+	default:
+		return false
+	}
+}
+
+// ToDecimal wei to decimals
+func ToDecimal(ivalue interface{}, decimals int) decimal.Decimal {
+	value := new(big.Int)
+	switch v := ivalue.(type) {
+	case string:
+		value.SetString(v, 10)
+	case *big.Int:
+		value = v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	num, _ := decimal.NewFromString(value.String())
+	result := num.Div(mul)
+
+	return result
+}
+
+// ToWei decimals to wei
+func ToWei(iamount interface{}, decimals int) *big.Int {
+	amount := decimal.NewFromFloat(0)
+	switch v := iamount.(type) {
+	case string:
+		amount, _ = decimal.NewFromString(v)
+	case float64:
+		amount = decimal.NewFromFloat(v)
+	case int64:
+		amount = decimal.NewFromFloat(float64(v))
+	case decimal.Decimal:
+		amount = v
+	case *decimal.Decimal:
+		amount = *v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	result := amount.Mul(mul)
+
+	wei := new(big.Int)
+	wei.SetString(result.String(), 10)
+
+	return wei
+}
+
+// CalcGasCost calculate gas cost given gas limit (units) and gas price (wei)
+func CalcGasCost(gasLimit uint64, gasPrice *big.Int) *big.Int {
+	gasLimitBig := big.NewInt(int64(gasLimit))
+	return gasLimitBig.Mul(gasLimitBig, gasPrice)
 }
