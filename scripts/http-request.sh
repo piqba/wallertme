80b3e267e942a8e1b5ff1db9e0b43897a3fcc1f4
kenriortega <kenriortega@gmail.com>
2021-10-11 10:54:21 -0400

    update this files

--- scripts/http-request.sh
+++ scripts/http-request.sh
@@ -1,10 +1,11 @@
-curl --location --request POST 'https://matic-mumbai.chainstacklabs.com/' \
+curl --location --request POST 'localhost:8545/' \
 --header 'Content-Type: application/json' \
 --data-raw '{
 	"jsonrpc":"2.0",
-	"method":"eth_getTransactionByHash",
+	"method":"eth_getBlockByNumber",
 	"params":[
-		"0x0314f52b94f624695e9035df6f76ba7c0209a57462ec6c9ade577523883fb681"
+		"latest",
+		true
 	],
 	"id":1
 }'
