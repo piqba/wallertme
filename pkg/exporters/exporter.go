b2791b0593936622691d9ef58cf8f92edb1d1d32
kenriortega <kenriortega@gmail.com>
2021-10-11 12:20:53 -0400

    feat: Define all method to interact with the exporters functions

---
+++ pkg/exporters/exporter.go
@@ -0,0 +1,108 @@
+package exporters
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-redis/redis/v8"
+	"github.com/jmoiron/sqlx"
+	"github.com/piqba/wallertme/pkg/logger"
+)
+
+const (
+	// JSONFILE ...
+	JSONFILE = "json"
+	// KAFKA ...
+	KAFKA = "kafka"
+	// POSTGRESQL ...
+	POSTGRESQL = "postgresql"
+	// REDIS ...
+	REDIS = "redis"
+)
+
+// Exporter ...
+type Exporter interface {
+	ExportData(data interface{})
+}
+
+// ExportToJSON ...
+func ExportToJSON(path, filename string, line interface{}) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		os.Mkdir(path, 0755)
+	}
+
+	file, err := os.OpenFile(filepath.Join(path, filename), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	datawriter := bufio.NewWriter(file)
+
+	switch line := line.(type) {
+	case string:
+		_, err = datawriter.WriteString(line + "\n")
+		if err != nil {
+			logger.LogError("failed to writer string: " + err.Error())
+		}
+	}
+
+	datawriter.Flush()
+	file.Close()
+}
+
+// ExportToRedisStream ...
+func ExportToRedisStream(rdb *redis.Client, key string, value map[string]interface{}) {
+
+	err := rdb.XAdd(context.TODO(), &redis.XAddArgs{
+		Stream: key,
+		Values: value,
+	}).Err()
+	if err != nil {
+		logger.LogError(err.Error())
+	}
+}
+
+// ExportToRedisStream ...
+func ExportTokafka(p *kafka.Producer, topic string, value string) {
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(value),
+	}, nil)
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+}
+
+func ExportToPostgresql(db *sqlx.DB, blockID int, data string) {
+	tx := db.MustBegin()
+	query := "INSERT INTO last_txs (blockid, data) VALUES ($1, $2) ON CONFLICT (blockid) DO NOTHING"
+
+	tx.MustExec(
+		query,
+		blockID,
+		data,
+	)
+	tx.Commit()
+}
