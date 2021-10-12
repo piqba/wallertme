62b3bc9d3e4aba9c8b93dc58e9be50d6a4a47f77
kenriortega <kenriortega@gmail.com>
2021-10-12 12:36:43 -0400

    docs: add logic for export data to json files

---
+++ examples/bb8/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	domain "github.com/piqba/wallertme/internal/bb8/domain"
+	"github.com/piqba/wallertme/pkg/exporters"
+	"github.com/piqba/wallertme/pkg/web3"
+)
+
+func main() {
+
+	repo := domain.NewTxRepository(exporters.JSONFILE, nil)
+
+	block, err := getTxByLatestBlock()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, txInBlock := range block.Result.Transactions {
+
+		tx := domain.ResultTx{
+			Time:          block.Result.Timestamp,
+			Txfrom:        txInBlock.From,
+			Txto:          txInBlock.To,
+			Gas:           txInBlock.Gas,
+			Gasprice:      txInBlock.GasPrice,
+			Block:         block.Result.Number,
+			Txhash:        txInBlock.Hash,
+			Value:         txInBlock.Value,
+			ContractTo:    "",
+			ContractValue: "",
+		}
+
+		err := repo.ExportData(tx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func getTxByLatestBlock() (web3.Block, error) {
+	api, err := web3.NewAPIEthClient(
+		web3.APIClientOptions{
+			Server: web3.GanacheDevNet,
+		},
+	)
+	if err != nil {
+		return web3.Block{}, err
+	}
+	payload := web3.PayloadReq{
+		Jsonrpc: "2.0",
+		Method:  "eth_getBlockByNumber",
+		Params: []interface{}{
+			"0x2",
+			true,
+		},
+		ID: 1,
+	}
+
+	blc, err := api.BlockByNumber(context.TODO(), payload)
+	if err != nil {
+		log.Fatal(err)
+		return web3.Block{}, err
+	}
+	return blc, nil
+}
