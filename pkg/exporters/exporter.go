c547e553567a25ed1dfe585da744868159faa29f
kenriortega <kenriortega@gmail.com>
2021-10-12 11:30:14 -0400

    ref: change method to return error

--- pkg/exporters/exporter.go
+++ pkg/exporters/exporter.go
@@ -11,6 +11,7 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
+	"github.com/piqba/wallertme/pkg/errors"
 	"github.com/piqba/wallertme/pkg/logger"
 )
 
@@ -27,11 +28,11 @@ const (
 
 // Exporter ...
 type Exporter interface {
-	ExportData(data interface{})
+	ExportData(data interface{}) error
 }
 
 // ExportToJSON ...
-func ExportToJSON(path, filename string, line interface{}) {
+func ExportToJSON(path, filename string, value interface{}) error {
 	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		os.Mkdir(path, 0755)
@@ -41,24 +42,27 @@ func ExportToJSON(path, filename string, line interface{}) {
 
 	if err != nil {
 		log.Fatalf("failed creating file: %s", err)
+		return err
 	}
 
 	datawriter := bufio.NewWriter(file)
 
-	switch line := line.(type) {
+	switch line := value.(type) {
 	case string:
 		_, err = datawriter.WriteString(line + "\n")
 		if err != nil {
 			logger.LogError("failed to writer string: " + err.Error())
+			return err
 		}
 	}
 
 	datawriter.Flush()
 	file.Close()
+	return nil
 }
 
 // ExportToRedisStream ...
-func ExportToRedisStream(rdb *redis.Client, key string, value map[string]interface{}) {
+func ExportToRedisStream(rdb *redis.Client, key string, value map[string]interface{}) error {
 
 	err := rdb.XAdd(context.TODO(), &redis.XAddArgs{
 		Stream: key,
@@ -66,11 +70,14 @@ func ExportToRedisStream(rdb *redis.Client, key string, value map[string]interfa
 	}).Err()
 	if err != nil {
 		logger.LogError(err.Error())
+		return err
 	}
+	return nil
 }
 
 // ExportToRedisStream ...
-func ExportTokafka(p *kafka.Producer, topic string, value string) {
+func ExportTokafka(p *kafka.Producer, topic string, value string) error {
+	var err error
 	// Delivery report handler for produced messages
 	go func() {
 		for e := range p.Events() {
@@ -78,8 +85,7 @@ func ExportTokafka(p *kafka.Producer, topic string, value string) {
 			case *kafka.Message:
 				if ev.TopicPartition.Error != nil {
 					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+					err = errors.Errorf("Delivery failed: %v\n", ev.TopicPartition)
 				}
 			}
 		}
@@ -93,16 +99,22 @@ func ExportTokafka(p *kafka.Producer, topic string, value string) {
 
 	// Wait for message deliveries before shutting down
 	p.Flush(15 * 1000)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func ExportToPostgresql(db *sqlx.DB, blockID int, data string) {
+// ExportToPostgresql ...
+func ExportToPostgresql(db *sqlx.DB, blockID int, value string) error {
 	tx := db.MustBegin()
 	query := "INSERT INTO last_txs (blockid, data) VALUES ($1, $2) ON CONFLICT (blockid) DO NOTHING"
 
 	tx.MustExec(
 		query,
 		blockID,
-		data,
+		value,
 	)
-	tx.Commit()
+	return tx.Commit()
+
 }
