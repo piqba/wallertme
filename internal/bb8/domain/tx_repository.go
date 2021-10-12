72f8cfc7354bc91c7a9147d4f34bcc49a375ab27
kenriortega <kenriortega@gmail.com>
2021-10-12 11:31:25 -0400

    feat: create repository method to export data

---
+++ internal/bb8/domain/tx_repository.go
@@ -0,0 +1,63 @@
+package bb8
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-redis/redis/v8"
+	"github.com/jmoiron/sqlx"
+	"github.com/piqba/wallertme/pkg/exporters"
+)
+
+var (
+	cwd, _ = os.Getwd()
+)
+
+type TxRepository struct {
+	exporterType string
+	clientRdb    *redis.Client
+	clientPgx    *sqlx.DB
+	clientKafka  *kafka.Producer
+}
+
+func NewTxRepository(exporterType string, clients ...interface{}) TxRepository {
+	var repo TxRepository
+	repo.exporterType = exporterType
+	for _, c := range clients {
+		switch c := c.(type) {
+		case *redis.Client:
+			repo.clientRdb = c
+		case *sqlx.DB:
+			repo.clientPgx = c
+		case *kafka.Producer:
+			repo.clientKafka = c
+
+		}
+	}
+	return repo
+}
+
+func (r *TxRepository) ExportData(data interface{}) error {
+	tx := data.(ResultTx)
+	switch r.exporterType {
+	case exporters.JSONFILE:
+		fileName := fmt.Sprintf("export_%d.json", time.Now().UnixNano())
+		return exporters.ExportToJSON(cwd, fileName, data)
+	case exporters.REDIS:
+		value, err := tx.ToMAP()
+		if err != nil {
+			return err
+		}
+		return exporters.ExportToRedisStream(r.clientRdb, exporters.TXS_STREAM_KEY, value)
+	case exporters.KAFKA:
+		value := tx.ToJSON()
+		return exporters.ExportTokafka(r.clientKafka, exporters.TXS_TOPIC_KEY, value)
+	case exporters.POSTGRESQL:
+		blockID := int(tx.Block)
+		value := tx.ToJSON()
+		return exporters.ExportToPostgresql(r.clientPgx, blockID, value)
+	}
+	return nil
+}
