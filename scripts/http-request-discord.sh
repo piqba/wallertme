00bf7d836bd795e488d95f181bb506963be01659
kenriortega <kenriortega@gmail.com>
2021-10-21 12:50:41 -0400

    feat: add script for make request to discord webhook

---
+++ scripts/http-request-discord.sh
@@ -0,0 +1,36 @@
+curl --location --request POST 'https://discordapp.com/api/webhooks/id' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "My Webhook Name",
+    "avatar_url": "",
+    "content": "The message to send",
+    "embeds": [
+    {
+      "author": {
+        "name": "Birdie♫",
+        "url": "https://www.reddit.com/r/cats/",
+        "icon_url": "https://i.imgur.com/R66g1Pe.jpg"
+      },
+      "title": "Title",
+      "url": "https://google.com/",
+      "description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
+      "color": 15258703,
+      "fields": [
+        {
+          "name": "Text",
+          "value": "More text",
+          "inline": true
+        },
+        {
+          "name": "Even more text",
+          "value": "Yup",
+          "inline": true
+        }
+      ],
+      "footer": {
+        "text": "Woah! So cool! :smirk:",
+        "icon_url": "https://i.imgur.com/fKL31aD.jpg"
+      }
+    }
+  ]
+}'
