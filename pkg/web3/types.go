1e90f3f6f6c0b36ddd5c186cad494b6f1c1ccfbf
kenriortega <kenriortega@gmail.com>
2021-10-23 09:12:00 -0400

    ref: add solana dev net endpoint & change payload struct name

--- pkg/web3/types.go
+++ pkg/web3/types.go
@@ -8,15 +8,14 @@ import (
 )
 
 const (
-	// Polygon
-	PolygonMainNet    = "https://polygon-rpc.com/"
-	PolygonMainNetWSS = "wss://ws-matic-mainnet.chainstacklabs.com"
-	PolygonTestNet    = "https://matic-mumbai.chainstacklabs.com/"
 	// localhost rpc
 	GanacheDevNet = "http://127.0.0.1:8545"
 	// Cardano ecosystem
 	CardanoTestNet = "https://explorer-api.testnet.dandelion.link"
 	CardanoMainNet = "https://explorer-api.mainnet.dandelion.link"
+
+	// Solana Ecosystem
+	SolanaDevNet = "https://api.devnet.solana.com"
 )
 
 // APIError is used to describe errors from the API.
@@ -28,14 +27,14 @@ func (e *APIError) Error() string {
 	return fmt.Sprintf("API Error, %+v", e.Response)
 }
 
-type PayloadReqEth struct {
+type PayloadReqJSONRPC struct {
 	Jsonrpc string        `json:"jsonrpc"`
 	Method  string        `json:"method"`
 	Params  []interface{} `json:"params"`
 	ID      int64         `json:"id"`
 }
 
-func (p *PayloadReqEth) ToReader() *strings.Reader {
+func (p *PayloadReqJSONRPC) ToReader() *strings.Reader {
 	byte, err := json.Marshal(p)
 	if err != nil {
 		log.Println(err)
