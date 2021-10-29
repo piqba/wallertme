d194786a2450f1e5e9ab551182d5c0bc4f17af03
kenriortega <kenriortega@gmail.com>
2021-10-29 18:27:19 -0400

    ref: add concurrency pattern and migrate logic to load data from json like supabase reponse

--- cmd/cli/commands/bb8.go
+++ cmd/cli/commands/bb8.go
@@ -2,11 +2,10 @@ package commands
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
 	domain "github.com/piqba/wallertme/internal/bb8/domain"
@@ -14,6 +13,7 @@ import (
 	"github.com/piqba/wallertme/pkg/errors"
 	"github.com/piqba/wallertme/pkg/exporters"
 	"github.com/piqba/wallertme/pkg/logger"
+	"github.com/piqba/wallertme/pkg/storage"
 	"github.com/piqba/wallertme/pkg/web3"
 	"github.com/spf13/cobra"
 )
@@ -23,14 +23,19 @@ const (
 	TxReceiver = "receiver"
 )
 
-var walletsJsonPath, _ = os.Getwd()
+var (
+	walletsJsonPath, _ = os.Getwd()
+)
 
 var producerCmd = &cobra.Command{
 	Use:   "bb8",
 	Short: "Publish Txs data from (SOLANA|CARDANO) blockchains to (REDIS)",
 	Run: func(cmd *cobra.Command, args []string) {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, os.Interrupt)
+
 		// flags
-		exporter, err := cmd.Flags().GetString(flagExporter)
+		source, err := cmd.Flags().GetString(flagDataSource)
 		if err != nil {
 			logger.LogError(errors.Errorf("bb8: %v", err).Error())
 		}
@@ -42,7 +47,6 @@ var producerCmd = &cobra.Command{
 		if err != nil {
 			logger.LogError(errors.Errorf("bb8: %v", err).Error())
 		}
-
 		walletsPath, err := cmd.Flags().GetString(flagWalletsPath)
 		if err != nil {
 			logger.LogError(errors.Errorf("bb8: %v", err).Error())
@@ -51,57 +55,67 @@ var producerCmd = &cobra.Command{
 		if err != nil {
 			logger.LogError(errors.Errorf("bb8: %v", err).Error())
 		}
+		// END FLAGS
 
-		// Options to load wallets
-		wallets, err := readWalletsJsonFile(walletsPath, walletsName)
+		// load wallets from source migrate to factory pattern
+		dataSource := storage.NewSource(source, storage.OptionsSource{
+			FileName: walletsName,
+			PathName: walletsPath,
+		})
+		wallets, err := dataSource.Wallets()
 		if err != nil {
 			logger.LogError(errors.Errorf("bb8: %v", err).Error())
 		}
+		// end Load wallets
 
-		quit := make(chan os.Signal, 1)
-
-		signal.Notify(quit, os.Interrupt)
-
-		exporterType := exporter
-
+		// Define repository to export data
 		var repo domain.TxRepository
-		switch exporterType {
-		case exporters.REDIS:
-			rdb := exporters.GetRedisDbClient()
-			repo = domain.NewTxRepository(exporters.REDIS, rdb)
-		case exporters.JSONFILE:
-			repo = domain.NewTxRepository(exporters.JSONFILE, nil)
+		rdb := exporters.GetRedisDbClient()
+		repo = domain.NewTxRepository(exporters.REDIS, rdb)
 
-		}
-
+		// Logic for watcher periodicaly
+		// and crtl+c
 		run := true
 
+		// parse timer duration flag
 		ds, err := time.ParseDuration(timer)
 		if err != nil {
 			logger.LogError(errors.Errorf("bb8: %v", err).Error())
 		}
+		// run periodicaly if watcher flag is true
 		for run {
 			select {
 			case sig := <-quit:
+
 				logger.LogInfo(fmt.Sprintf("bb8: app is shutting down %v", sig.String()))
 				_, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 				run = false
 				defer cancel()
+
 			case <-time.Tick(ds):
-				for _, w := range wallets {
-					Exce(repo, w, exporterType)
+
+				var wg sync.WaitGroup
+				wg.Add(len(wallets))
+				for _, it := range wallets {
+					go func(wallet storage.Wallet) {
+						Exce(repo, wallet)
+						wg.Done()
+					}(it)
 				}
+				wg.Wait()
+
 				if !watcher {
 					logger.LogInfo(fmt.Sprintf("bb8: app is shutting down %v", "closeapp"))
 					run = false
 				}
+
 			}
 		}
 	},
 }
 
 func init() {
-	producerCmd.Flags().String(flagExporter, exporters.REDIS, "select a exporter to send data")
+	producerCmd.Flags().String(flagDataSource, "json", "select a wallets data source from (json|db)")
 	producerCmd.Flags().String(flagTimer, "1s", "select a time duration to watch all txs")
 	producerCmd.Flags().Bool(flagWatcher, false, "select true|false if you want to run this task periodicaly")
 	producerCmd.Flags().String(flagWalletsPath, walletsJsonPath, "select the path of wallet.json file")
@@ -110,45 +124,31 @@ func init() {
 
 }
 
-func readWalletsJsonFile(path, filename string) ([]map[string]interface{}, error) {
-	jsonFile, err := os.Open(fmt.Sprintf("%s/%s", path, filename))
-	if err != nil {
-		return nil, err
-	}
+// Exce excecute proccessing functions
+func Exce(repo domain.TxRepository, wallet storage.Wallet) {
 
-	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	symbol := wallet.Symbol
+	address := wallet.Address
+	networkType := wallet.NetworkType
+	isActive := wallet.IsActive
 
-	var result []map[string]interface{}
-	err = json.Unmarshal([]byte(byteValue), &result)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
+	if isActive {
 
-func Exce(repo domain.TxRepository, wallet map[string]interface{}, exporterType string) {
+		switch symbol {
+		case "ADA":
+			addrInfo := lastTXForADA(networkType, address)
 
-	symbol := wallet["symbol"].(string)
-	address := wallet["address"].(string)
-	networkType := wallet["networkType"].(string)
-
-	switch symbol {
-	case "ADA":
-		addrInfo := lastTXForADA(networkType, address)
-
-		lastTX := addrInfo.Result.CATxList[len(addrInfo.Result.CATxList)-1]
-		tx := domain.ResultLastTxADA{
-			Addr:          address,
-			CtbID:         lastTX.CtbID,
-			CtbTimeIssued: fmt.Sprintf("%d", lastTX.CtbTimeIssued),
-			FromAddr:      lastTX.CtbOutputs[0].CtaAddress,
-			ToAddr:        lastTX.CtbOutputs[1].CtaAddress,
-			Balance:       addrInfo.Result.CABalance.GetCoin,
-			Ammount:       lastTX.CtbOutputs[1].CtaAmount.GetCoin,
-		}
-		switch exporterType {
-		case exporters.REDIS:
+			lastTX := addrInfo.Result.CATxList[len(addrInfo.Result.CATxList)-1]
+			tx := domain.ResultLastTxADA{
+				Addr:          address,
+				CtbID:         lastTX.CtbID,
+				CtbTimeIssued: fmt.Sprintf("%d", lastTX.CtbTimeIssued),
+				FromAddr:      lastTX.CtbOutputs[0].CtaAddress,
+				ToAddr:        lastTX.CtbOutputs[1].CtaAddress,
+				Balance:       addrInfo.Result.CABalance.GetCoin,
+				Ammount:       lastTX.CtbOutputs[1].CtaAmount.GetCoin,
+				Symbol:        symbol,
+			}
 
 			lastTXFromRdb, err := repo.Get(context.TODO(), address)
 			if err != nil {
@@ -176,30 +176,24 @@ func Exce(repo domain.TxRepository, wallet map[string]interface{}, exporterType 
 				}
 
 			}
-		case exporters.JSONFILE:
-			err := repo.ExportData(tx, symbol)
-			if err != nil {
-				logger.LogError(errors.Errorf("bb8: %s", err).Error())
+
+		case "SOL":
+			addrInfo := lastTXForSOL(networkType, address)
+			tx := domain.ResultLastTxSOL{
+				Addr:      address,
+				TxID:      addrInfo.Result.Transaction.Signatures[0],
+				Timestamp: fmt.Sprintf("%v", addrInfo.Result.BlockTime),
+				FromAddr:  addrInfo.Result.Transaction.Message.AccountKeys[0],
+				ToAddr:    addrInfo.Result.Transaction.Message.AccountKeys[1],
+				Balance:   fmt.Sprintf("%v", addrInfo.Result.Meta.PostBalances[0]),
+				Ammount:   fmt.Sprintf("%v", addrInfo.Result.Meta.PreBalances[0]-addrInfo.Result.Meta.PostBalances[0]),
+				Symbol:    symbol,
 			}
-		}
-	case "SOL":
-		addrInfo := lastTXForSOL(networkType, address)
-		tx := domain.ResultLastTxSOL{
-			Addr:      address,
-			TxID:      addrInfo.Result.Transaction.Signatures[0],
-			Timestamp: fmt.Sprintf("%v", addrInfo.Result.BlockTime),
-			FromAddr:  addrInfo.Result.Transaction.Message.AccountKeys[0],
-			ToAddr:    addrInfo.Result.Transaction.Message.AccountKeys[1],
-			Balance:   fmt.Sprintf("%v", addrInfo.Result.Meta.PostBalances[0]),
-			Ammount:   fmt.Sprintf("%v", addrInfo.Result.Meta.PreBalances[0]-addrInfo.Result.Meta.PostBalances[0]),
-		}
-		if address == tx.FromAddr {
-			tx.TypeTx = TxSender
-		} else if address == tx.ToAddr {
-			tx.TypeTx = TxReceiver
-		}
-		switch exporterType {
-		case exporters.REDIS:
+			if address == tx.FromAddr {
+				tx.TypeTx = TxSender
+			} else if address == tx.ToAddr {
+				tx.TypeTx = TxReceiver
+			}
 
 			lastTXFromRdb, err := repo.Get(context.TODO(), address)
 			if err != nil {
@@ -227,16 +221,15 @@ func Exce(repo domain.TxRepository, wallet map[string]interface{}, exporterType 
 				}
 
 			}
-		case exporters.JSONFILE:
-			err := repo.ExportData(tx, symbol)
-			if err != nil {
-				logger.LogError(errors.Errorf("bb8: %s", err).Error())
-			}
+
 		}
+	} else {
+		// isActive false
 	}
 
 }
 
+// lastTXForADA return las TX from Cardano
 func lastTXForADA(networkType, address string) web3.AddrSumary {
 	cardano, err := web3.NewAPICardanoClient(web3.APIClientOptions{
 		NetworkType: networkType,
@@ -252,6 +245,8 @@ func lastTXForADA(networkType, address string) web3.AddrSumary {
 
 	return sumary
 }
+
+// lastTXForSOL return las TX from Solana
 func lastTXForSOL(networkType, address string) web3.TxInfo {
 	solanaApi, err := web3.NewAPISolanaClient(web3.APIClientOptions{
 		NetworkType: networkType,
