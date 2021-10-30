a101edd9ef5c36dd7f61fb33953bc9ef0d4aa09c
kenriortega <kenriortega@gmail.com>
2021-10-30 13:27:51 -0400

    docs: add example of postgres integration datasource

---
+++ examples/supabase/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/joho/godotenv"
+	"github.com/piqba/wallertme/pkg/errors"
+	"github.com/piqba/wallertme/pkg/logger"
+	"github.com/piqba/wallertme/pkg/storage"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		logger.LogError(errors.Errorf("main:%s", err).Error())
+	}
+	numcpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(numcpu) // Try to use all available CPUs.
+}
+
+func main() {
+	pgx, err := storage.PostgreSQLConnection()
+	if err != nil {
+		logger.LogError(errors.Errorf("bb8: %v", err).Error())
+
+	}
+	dataSource := storage.NewSource("db", storage.OptionsSource{
+		Pgx: pgx,
+	})
+	wallets, err := dataSource.Wallets()
+	if err != nil {
+		logger.LogError(errors.Errorf("bb8: %v", err).Error())
+	}
+	for _, w := range wallets {
+		fmt.Println(w.ToJSON())
+	}
+}
