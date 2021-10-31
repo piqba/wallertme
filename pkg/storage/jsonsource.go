c4f4f5fe820f7aaa36423d01a0fa3a5b8c269602
kenriortega <kenriortega@gmail.com>
2021-10-31 09:24:44 -0400

    ref(pkg.storage): add otel functionalities

--- pkg/storage/jsonsource.go
+++ pkg/storage/jsonsource.go
@@ -1,16 +1,25 @@
 package storage
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 )
 
 // WalletsFromJsonToMAP read json filename and path
-func WalletsFromJsonToMAP(path, filename string) ([]map[string]interface{}, error) {
+func WalletsFromJsonToMAP(ctx context.Context, path, filename string) ([]map[string]interface{}, error) {
+	_, span := otel.Tracer(nameSourcerJson).Start(ctx, "WalletsFromJsonToMAP")
+	defer span.End()
 	jsonFile, err := os.Open(fmt.Sprintf("%s/%s", path, filename))
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
 
@@ -20,15 +29,23 @@ func WalletsFromJsonToMAP(path, filename string) ([]map[string]interface{}, erro
 	var result []map[string]interface{}
 	err = json.Unmarshal([]byte(byteValue), &result)
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
+	span.SetAttributes(attribute.Int("jsonfile.query.result", len(result)))
+
 	return result, nil
 }
 
 // WalletsFromJsonToStruct read json filename and path
-func WalletsFromJsonToStruct(path, filename string) ([]Wallet, error) {
+func WalletsFromJsonToStruct(ctx context.Context, path, filename string) ([]Wallet, error) {
+	_, span := otel.Tracer(nameSourcerJson).Start(ctx, "WalletsFromJsonToStruct")
+	defer span.End()
 	jsonFile, err := os.Open(fmt.Sprintf("%s/%s", path, filename))
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
 
@@ -38,7 +55,11 @@ func WalletsFromJsonToStruct(path, filename string) ([]Wallet, error) {
 	var result []Wallet
 	err = json.Unmarshal([]byte(byteValue), &result)
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
+	span.SetAttributes(attribute.Int("jsonfile.query.result", len(result)))
+
 	return result, nil
 }
